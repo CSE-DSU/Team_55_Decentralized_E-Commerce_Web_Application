844601032a529483004b59362a8005a424a58eca
habib-ahmed-00 <hahmed.y2k@gmail.com>
2023-03-27 19:10:05 +0530

    Contracts Deployed

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,103 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    // Defines a local DS for the products
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    // Defines order
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
+    // Mapping each item to unique key
+    mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
+
+    event List(string name, uint256 cost, uint256 quantity);
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
+
+    modifier onlyOwner() {
+        //  Restrict listing to the owner
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() {
+        // Person who is sending the contract
+        owner = msg.sender;
+    }
+
+    // List products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner {
+        //  Create Item Struct
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        // Save Item Struct
+        items[_id] = item;
+
+        // Emit Event on BLOCKCHAIN
+        emit List(_name, _cost, _stock);
+    }
+
+    // Buy Products
+    function buy(uint256 _id) public payable {
+        // Receive Crypto: Payable
+
+        // Fetch item
+        Item memory item = items[_id];
+
+        // Require enough ether to buy the item
+        require(msg.value >= item.cost);
+
+        // Require item is in stock
+        require(item.stock > 0);
+
+        // Create an order
+        Order memory order = Order(block.timestamp, item);
+
+        // Add order for user
+        orderCount[msg.sender]++;
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        // Substract Stock
+        items[_id].stock = item.stock - 1;
+
+        // Emit event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+    }
+
+    // Withdraw Funds
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
+}
